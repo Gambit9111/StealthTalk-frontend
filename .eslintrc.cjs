6f0eec6ffc4b8bd0d7e65cacb167d7e445f9f092
user1s <bitgam911@gmail.com>
2023-03-21 19:24:56 +0200

    a

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  // overrides: [
-  //   {
-  //     extends: [
-  //       "plugin:@typescript-eslint/recommended-requiring-type-checking",
-  //     ],
-  //     files: ["*.ts", "*.tsx"],
-  //     parserOptions: {
-  //       project: "tsconfig.json",
-  //     },
-  //   },
-  // ],
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+    },
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
